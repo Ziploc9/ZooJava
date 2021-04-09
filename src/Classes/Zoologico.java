2905dc4a213aed4c07e93e6747a98d973d55fa8f
Main <vallejomartin6@gmail.com>
2021-04-08 21:34:03 -0300

    agregado de clases

---
+++ src/Classes/Zoologico.java
@@ -0,0 +1,15 @@
+package Classes;
+
+public class Zoologico {
+
+    private int empleados;
+    private double dimensiones;
+    private int jaulas;
+    private String nombre;
+    private boolean abierto;
+    private String direccion;
+
+
+
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,7 @@
 package com.company;
+import Classes.* ;
+
+
 
 public class Main {
 
